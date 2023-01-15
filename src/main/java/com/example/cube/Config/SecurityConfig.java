e90cf838f44a4230fec93b0c9590c2eb337a0ca3
Valentin <75315884+Contunder@users.noreply.github.com>
2023-01-15 23:41:48 +0100

    Remove useless statics

--- src/main/java/com/example/cube/Config/SecurityConfig.java
+++ src/main/java/com/example/cube/Config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
