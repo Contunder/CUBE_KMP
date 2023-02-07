f9ace2e8b4dbd30e3c7b438b837bc03a61f37ee0
Valentin <75315884+Contunder@users.noreply.github.com>
2023-02-07 17:34:55 +0100

    Update CORSFilter.java

--- src/main/java/com/example/cube/Config/CORSFilter.java
+++ src/main/java/com/example/cube/Config/CORSFilter.java
@@ -2,7 +2,12 @@ package com.example.cube.Config;
 
 import java.io.IOException;
 
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
