f14cdb4942b039b23adec30c361df9ccc4557788
valentin <valentin.denavaut@hotmail.fr>
2023-02-08 11:37:52 +0100

    Update CORSFilter.java

--- src/main/java/com/example/cube/Config/CORSFilter.java
+++ src/main/java/com/example/cube/Config/CORSFilter.java
@@ -33,7 +33,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         chain.doFilter(req, res);
     }
