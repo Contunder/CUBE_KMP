ac6fa4660968a25135ba0d231ef3419f4254254c
Valentin <75315884+Contunder@users.noreply.github.com>
2023-07-06 14:11:46 +0200

    Add Analytics Test

---
+++ src/test/java/com/example/cube/Service/impl/AnalyticsServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.example.cube.Service.impl;
+
+import com.example.cube.Model.Analytics;
+import com.example.cube.Repository.AnalyticsRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AnalyticsServiceImplTest {
+
+    @Mock
+    private AnalyticsRepository analyticsRepository;
+
+    @InjectMocks
+    private AnalyticsServiceImpl analyticsService;
+
+    @Test
+    void testGetAnalyticsByDate() {
+        // Arrange
+        Date date = new Date(2023,07,06);
+        Analytics expectedAnalytics = new Analytics();
+
+        when(analyticsRepository.getAnalyticsByDate(date)).thenReturn(Optional.of(expectedAnalytics));
+
+        // Act
+        Optional<Analytics> result = analyticsService.getAnalyticsByDate(date);
+
+        // Assert
+        assertTrue(result.isPresent());
+        assertSame(expectedAnalytics, result.get());
+    }
+
+}
